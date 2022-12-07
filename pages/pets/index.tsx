5fa519298e4ac66d5752ebf6ca0e8e03177b2aa0
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-12-07 23:03:15 +0100

    check blur option for Image component from Next

--- pages/pets/index.tsx
+++ pages/pets/index.tsx
@@ -1,16 +1,29 @@
 import Image from "next/image";
+import Img1 from "../../public/images/1.jpg";
 
 const Pets = () => {
   return (
     <div>
+      <div>
+        <Image
+          src={Img1}
+          placeholder="blur"
+          //   special option for more optimization
+          //   blurDataURL=""
+          alt="pet"
+          width={"280px"}
+          height={"420px"}
+        />
+      </div>
+
       {["1", "2", "3"].map((path) => {
         return (
           <div key={path}>
             <Image
               src={`/images/${path}.jpg`}
               alt={`pets animal ${path}.jpg`}
-              width={"200px"}
-              height={"300px"}
+              width={"280px"}
+              height={"420px"}
             />
           </div>
         );

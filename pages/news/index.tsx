78a9ccb8cc63690d7ea260ef17055365883f49a7
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-12-09 09:34:13 +0100

    handle env variables with .env.local for serverSideProps and kinda public env with prefix NEXT_PUBLIC_<ENV-NAME>

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -12,11 +12,11 @@ export interface INews {
 }
 
 const ListNews = ({ news }: INews) => {
-  console.log("🚀 ~ file: index.tsx:14 ~ ListNews ~ news", news);
-
   return (
     <div>
       <h1> list of NEWS</h1>
+
+      <pre>{process.env.NEXT_PUBLIC_ANALITICS}</pre>
 
       <ul>
         {news.map((nw) => {
@@ -36,12 +36,23 @@ const ListNews = ({ news }: INews) => {
 export default ListNews;
 
 export async function getServerSideProps() {
+  const API_URL = process.env.API_URL;
+  console.log(
+    "🚀 ~ file: index.tsx:40 ~ getServerSideProps ~ API_URL",
+    API_URL
+  );
+  const DB_USER = process.env.DB_USER;
+  console.log(
+    "🚀 ~ file: index.tsx:41 ~ getServerSideProps ~ DB_USER",
+    DB_USER
+  );
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  console.log(
+    "🚀 ~ file: index.tsx:42 ~ getServerSideProps ~ DB_PASSWORD",
+    DB_PASSWORD
+  );
   const resp = await fetch("http://localhost:4000/news");
   const data = await resp.json();
-  console.log(
-    "🚀 ~ file: index.tsx:29 ~ GetServerSidePropsContext ~ data",
-    data
-  );
 
   return {
     props: {
